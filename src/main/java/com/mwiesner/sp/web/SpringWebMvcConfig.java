e3836630b29469834952c792f59f3b531d98bbc0
Mike Wiesner <mwiesner@gopivotal.com>
2013-09-06 14:33:54 +0200

    Minor change

--- src/main/java/com/mwiesner/sp/web/SpringWebMvcConfig.java
+++ src/main/java/com/mwiesner/sp/web/SpringWebMvcConfig.java
@@ -2,7 +2,6 @@ package com.mwiesner.sp.web;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
